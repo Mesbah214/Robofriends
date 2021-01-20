d51b172449218b67af2404e117cf4c5ddbbfc53f
Mesbah <msbh214@gmail.com>
2021-01-20 23:05:43 +0600

    added single card

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robot" src="https://robohash.org/text?200x200" />
+
+      <div>
+        <h2>John Doe</h2>
+        <p>john.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
